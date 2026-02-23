02b5cadaa0d66454c724025bb1d466cfeac6bc5b
Gamin <Gamin@ASTREK>
2026-02-24 00:55:18 +0800

    new file:   OptiSched/app/(dashboard)/mainDashboard.jsx

---
+++ OptiSched/app/(dashboard)/mainDashboard.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import React from 'react'
+
+const mainDashboard = () => {
+  return (
+    <View>
+      <Text>mainDashboard</Text>
+    </View>
+  )
+}
+
+export default mainDashboard
+
+const styles = StyleSheet.create({})
