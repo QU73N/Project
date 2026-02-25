d0220d37e102168cd6044be680387415b6d5f29f
Gamin <Gamin@ASTREK>
2026-02-25 21:18:08 +0800

    modified:   OptiSched/backend/.env 	new file:   OptiSched/backend/src/config/cron.js 	modified:   OptiSched/backend/src/config/env.js

--- OptiSched/backend/src/config/env.js
+++ OptiSched/backend/src/config/env.js
@@ -1,7 +1,8 @@
-import dotenv from 'dotenv/config'
+import "dotenv/config";
 
 export const ENV = {
-    PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL,
-    NODE_ENV: process.env.NODE_ENV
-}
+  PORT: process.env.PORT || 5001,
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  API_URL: process.env.API_URL,
+};
